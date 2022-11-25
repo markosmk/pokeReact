f8fbfcdbe447eba45a17583d9056275c6533df99
MarkosMk <markosmk@me.com>
2022-11-24 22:51:57 -0300

    chore: App updated, added tailwind index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import { Pokemon } from './components/pokemon.jsx';
+import { getPokemons } from './services/getPokemons.js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState(null);
+
+  useEffect(() => {
+    getPokemons().then((data) => setPokemons(data));
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="p-4">
+      <h1 className="text-3xl font-bold mb-4">All pokemons!</h1>
+      <ul className="grid md:grid-cols-2 gap-10 container mx-auto">
+        {pokemons &&
+          pokemons.map((item) => {
+            return <Pokemon key={item.id} {...item} />;
+          })}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
