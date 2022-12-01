7a4a41069ed092c14c61aca5460f39f38eca064e
MarkosMk <markosmk@me.com>
2022-11-30 23:02:51 -0300

    feat: component stateless

---
+++ src/components/ItemMenu.jsx
@@ -0,0 +1,17 @@
+// @ts-ignore
+import ball from '../assets/pokeball.svg';
+
+export const ItemMenu = ({ item }) => {
+  return (
+    <div
+      className="relative h-16 rounded-2xl text-white flex items-center justify-start pl-4 overflow-hidden bottom-1 active:scale-95 active:-bottom-1 transition-all"
+      style={{
+        backgroundColor: item.color,
+        boxShadow: '0px 20px 32px -16px ' + item.color,
+      }}
+    >
+      <img className="absolute -right-1/2 -top-1/2 opacity-10 w-40" src={ball} alt="" />
+      <h3 className="font-bold text-lg z-10">{item.title}</h3>
+    </div>
+  );
+};
--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,11 +1,42 @@
+// @ts-ignore
+import ball from '../assets/pokeball.svg';
+
 export const Pokemon = ({ name, image, types, color }) => {
   return (
-    <li className="flex flex-col shadow-2xl p-4 rounded-2xl h-52 relative border-slate-200 border mb-4 md:mb-0">
-      <h3 className={`mb-2 font-bold text-7xl md:text-5xl capitalize ${color}`}>{name}</h3>
-      <p className="text-slate-500 font-medium capitalize text-xl">
-        {color} {types[0].type.name}
-      </p>
-      <img src={image} alt="alt" className="absolute -bottom-6 right-4 w-64 sm:w-52 sm:-right-8" />
+    <li className="flex flex-col p-4 rounded-2xl h-32 relative bg-green-500 overflow-hidden">
+      <h3 className={`mb-2 font-bold text-white  capitalize ${color}`}>{name}</h3>
+      <ul className="flex flex-col gap-1">
+        {types.map((item) => (
+          <li
+            key={item.slot}
+            className="text-white rounded-full bg-white text-xs bg-opacity-30 font-medium capitalize px-3 py-1 w-min"
+          >
+            {item.type.name}
+          </li>
+        ))}
+      </ul>
+      <img src={image} alt="alt" className="absolute bottom-2 right-2 w-20 z-10" />
+      <img src={ball} alt="alt" className="absolute -right-12 -bottom-20 opacity-5 w-64" />
+    </li>
+  );
+};
+
+const PokemonBackup = ({ name, image, types, color }) => {
+  return (
+    <li className="flex flex-col shadow-2xl p-8 rounded-2xl h-52 relative bg-green-500 overflow-hidden">
+      <h3 className={`mb-2 font-bold text-white text-2xl capitalize ${color}`}>{name}</h3>
+      <ul className="flex flex-col gap-2">
+        {types.map((item) => (
+          <li
+            key={item.slot}
+            className="text-white rounded-full bg-white bg-opacity-30 font-medium capitalize px-4 py-1 w-min"
+          >
+            {item.type.name}
+          </li>
+        ))}
+      </ul>
+      <img src={image} alt="alt" className="absolute bottom-4 right-4 w-32 z-10" />
+      <img src={ball} alt="alt" className="absolute -right-12 -bottom-20 opacity-5 w-64" />
     </li>
   );
 };
