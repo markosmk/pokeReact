8186c63aebbf746c1ff249b0fc59990541096c59
MarkosMk <markosmk@me.com>
2022-12-02 21:54:35 -0300

    feat: add react router dom

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
-  //   alias: [
-  //     {
-  //       find: "src",
-  //       // replacement: resolve(__dirname, "src"),
-  //     },
-  //   ],
-  // },
+  resolve: {
+    alias: [
+      {
+        find: 'src',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
   plugins: [react()],
 });
